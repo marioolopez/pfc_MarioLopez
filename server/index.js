709903e72f3dc859b6c1a8848f08e707e5ccac94
marioolopez <150073715+marioolopez@users.noreply.github.com>
2025-09-23 20:26:50 +0200

    Primer commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+dotenv.config();//👈 primero, antes de importar nada más
+console.log("ENV:", process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS);
+
+
+import express from "express";
+import cors from "cors";
+import sequelize from "./database.js";
+import usuarioRoutes from "./routes/usuario.routes.js";
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", usuarioRoutes);
+app.get("/", (req, res) => res.send("🚀 API Autoescuela lista"));
+
+
+const PORT = process.env.PORT || 4000;
+(async () => {
+  try {
+    console.log("DB CONFIG:", process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS);
+    await sequelize.authenticate();
+    console.log("✅ Conectado a MySQL");
+    await sequelize.sync();
+    app.listen(PORT, () =>
+      console.log(`Servidor corriendo en http://localhost:${PORT}`)
+    );
+  } catch (err) {
+    console.error("❌ Error en DB:", err);
+  }
+})();
